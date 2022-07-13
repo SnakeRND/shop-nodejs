50b10ac05b5e99d9cb1b73cdc3f527ed65a11b7c
Alexandr Androsov <alexandr.v.androsov@gmail.com>
2022-07-13 13:21:56 +0300

    refactoring

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router
-const userController = require('../controllers/userController')
+const userController = require('../controllers/UserController')
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
