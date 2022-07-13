4f38a5f7b9b900f085a77181e6fb0406438714c4
Alexandr Androsov <alexandr.v.androsov@gmail.com>
2022-07-13 12:59:05 +0300

    routes

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const router = require('./routes/index')
+const {request} = require("express");
+
 const PORT = process.env.PORT || 5090
 const app = express()
+
 const start = async () => {
     try {
         await sequelize.authenticate()
@@ -13,5 +19,13 @@ const start = async () => {
         console.log(e)
     }
 }
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+app.get('/', (request, response) => {
+    response.status(200).json({message: 'It works'})
+})
 
 start()
---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router = new Router
+const userRouter = require('./userRouter')
+const deviceRouter = require('./deviceRouter')
+const typeRouter = require('./typeRouter')
+const brandRouter = require('./brandRouter')
+
+router.use('/user', userRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviceRouter)
+router.use('/type', typeRouter)
+
+module.exports = router
