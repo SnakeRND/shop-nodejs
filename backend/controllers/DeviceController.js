f9c7594f52cfef6213fe34afcb6108c0b159f640
Alexandr Androsov <alexandr.v.androsov@gmail.com>
2022-07-13 15:06:12 +0300

    methods for devices

--- backend/controllers/DeviceController.js
+++ backend/controllers/DeviceController.js
@@ -1,14 +1,71 @@
-class DeviceController {
-    async create(request, response) {
+const uuid = require('uuid')
+const path = require('path')
+const {Device, DeviceInfo} = require('../models/models')
+const ApiError = require('../errors/apiError')
 
+class DeviceController {
+    async create(request, response, next) {
+        try {
+            let {name, price, brandId, typeId, info} = request.body
+            const {image} = request.files
+
+            let fileName = uuid.v4() + ".jpg"
+            image.mv(path.resolve(__dirname, '..', 'uploads', fileName))
+
+            const device = await Device.create({name, price, brandId, typeId,image: fileName, info})
+
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                )
+            }
+
+            return response.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
     }
 
     async getAll(request, response) {
+        let {brandId, typeId, limit, page} = request.query
+        let devices
 
+        page = page || 1
+        limit = limit || 9
+
+        let offset = page * limit - limit
+
+        if (!brandId && !typeId) {
+            devices = await Device.findAndCountAll({limit, offset})
+        }
+        if (brandId && !typeId) {
+            devices = await Device.findAndCountAll({where: {brandId}, limit, offset})
+        }
+        if (!brandId && typeId) {
+            devices = await Device.findAndCountAll({where: {typeId}, limit, offset})
+        }
+        if (brandId && typeId) {
+            devices = await Device.findAndCountAll({where: {brandId, typeId}, limit, offset})
+        }
+
+        return response.json(devices)
     }
 
     async getById(request, response) {
+        const {id} = request.params
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            },
+        )
 
+        return response.json(device)
     }
 }
 
--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const {request} = require("express")
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5090
 const app = express()
@@ -23,6 +25,8 @@ const start = async () => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'uploads')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 // last middleware
